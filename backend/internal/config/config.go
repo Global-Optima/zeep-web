a5ac2106925759951860734a85d2945db2c20d7a
Ramazan Bakytuly <97844249+maulerrr@users.noreply.github.com>
2024-10-30 00:16:08 +0500

    setup: created folder structure of a backend (#3)
    * setup: created folder structure of a backend
    
    * setup: add .gitkeep to track empty folder
    
    * setup: added configuration loading, documentation template

---
+++ backend/internal/config/config.go
@@ -0,0 +1,44 @@
+package config
+
+import (
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	Database DatabaseConfig
+	JWT      JWTConfig
+	Server   ServerConfig
+}
+
+type DatabaseConfig struct {
+	Host     string
+	Port     int
+	User     string
+	Password string
+	Name     string
+}
+
+type JWTConfig struct {
+	SecretKey string
+}
+
+type ServerConfig struct {
+	Port int
+}
+
+var AppConfig Config
+
+func LoadConfig() {
+	viper.SetConfigFile("config.yml")
+	viper.AddConfigPath(".")
+
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalf("Error reading config file, %s", err)
+	}
+
+	if err := viper.Unmarshal(&AppConfig); err != nil {
+		log.Fatalf("Unable to decode into struct, %v", err)
+	}
+}
