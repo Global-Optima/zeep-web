5c9c3d473f6c643f003388e1ac5f29a779ca81d3
Diar Begisbayev <diar_beg64@mail.ru>
2024-11-13 10:13:43 +0500

    feat: added vite and images compression in frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,6 +6,8 @@ import autoprefixer from 'autoprefixer'
 import { dirname, resolve } from 'node:path'
 import tailwind from 'tailwindcss'
 import { defineConfig } from 'vite'
+import viteCompression from 'vite-plugin-compression'
+import viteImagemin from 'vite-plugin-imagemin'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
@@ -60,6 +62,34 @@ export default defineConfig({
 				enabled: true,
 			},
 		}),
+		viteImagemin({
+			gifsicle: {
+				optimizationLevel: 7,
+				interlaced: false,
+			},
+			optipng: {
+				optimizationLevel: 7,
+			},
+			mozjpeg: {
+				quality: 20,
+			},
+			pngquant: {
+				quality: [0.8, 0.9],
+				speed: 4,
+			},
+			svgo: {
+				plugins: [
+					{
+						name: 'removeViewBox',
+					},
+					{
+						name: 'removeEmptyAttrs',
+						active: false,
+					},
+				],
+			},
+		}),
+		viteCompression(),
 	],
 	resolve: {
 		alias: {
