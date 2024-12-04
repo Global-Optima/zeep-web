e4931206686676119ed1e78d9913b3d9ee4a5f41
maulerrr <211239@astanait.edu.kz>
2024-12-04 11:31:05 +0500

    refactor: camelcase as requested

--- backend/internal/modules/orders/types/orders.event.go
+++ backend/internal/modules/orders/types/orders.event.go
@@ -8,27 +8,27 @@ import (
 
 type OrderEvent struct {
 	ID                uint            `json:"id"`
-	CustomerName      string          `json:"customer_name"`
+	CustomerName      string          `json:"customerName"`
 	ETA               time.Time       `json:"eta"`
-	OrderType         string          `json:"order_type"` // "Доставка" or "Кафе"
-	StoreID           uint            `json:"store_id"`
-	SubOrdersQuantity int             `json:"sub_orders_quantity"`
+	OrderType         string          `json:"orderType"` // "Доставка" or "Кафе"
+	StoreID           uint            `json:"storeId"`
+	SubOrdersQuantity int             `json:"subOrdersQuantity"`
 	Items             []SubOrderEvent `json:"items"`
 	Status            string          `json:"status"`
 	Timestamp         time.Time       `json:"timestamp"`
 }
 
 type SubOrderEvent struct {
-	SubOrderID    uint             `json:"sub_order_id"`
-	ProductSizeID uint             `json:"product_size_id"`
-	ProductName   string           `json:"product_name"`
+	SubOrderID    uint             `json:"subOrderId"`
+	ProductSizeID uint             `json:"productSizeId"`
+	ProductName   string           `json:"productName"`
 	Additives     []AdditiveDetail `json:"additives"`
 	ETA           time.Time        `json:"eta"`
 	Status        data.OrderStatus `json:"status"`
 }
 
 type AdditiveDetail struct {
-	AdditiveID uint    `json:"additive_id"`
+	AdditiveID uint    `json:"additiveId"`
 	Name       string  `json:"name"`
 	Price      float64 `json:"price"`
 }
