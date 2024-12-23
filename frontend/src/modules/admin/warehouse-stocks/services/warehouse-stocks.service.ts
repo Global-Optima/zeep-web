3e8c1d4b3c034a5f3087997f316cba496c9b967e
Diar Begisbayev <diar_beg64@mail.ru>
2024-12-24 02:42:31 +0500

    feat: fixed linting errors

--- frontend/src/modules/admin/warehouse-stocks/services/warehouse-stocks.service.ts
+++ frontend/src/modules/admin/warehouse-stocks/services/warehouse-stocks.service.ts
@@ -1,7 +1,10 @@
 import { apiClient } from '@/core/config/axios-instance.config'
 import type { PaginatedResponse } from '@/core/utils/pagination.utils'
 import { buildRequestFilter } from '@/core/utils/request-filters.utils'
-import type { GetWarehouseStockFilter, InventoryLevelsResponse } from '../models/warehouse-stock.model'
+import type {
+	GetWarehouseStockFilter,
+	InventoryLevelsResponse,
+} from '../models/warehouse-stock.model'
 
 class WarehouseStocksService {
 	async getWarehouseStocks(filter?: GetWarehouseStockFilter) {
