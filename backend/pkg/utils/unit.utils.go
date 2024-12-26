16cd0999c8eaa5dddb140d9f47f8a37aa22cf084
maulerrr <211239@astanait.edu.kz>
2024-12-26 10:00:29 +0500

    refactor: separate sku category

--- backend/pkg/utils/unit.utils.go
+++ backend/pkg/utils/unit.utils.go
@@ -1,5 +1,28 @@
 package utils
 
-func ConvertPackagesToUnits(packageSize, quantityInPackages, conversionFactor float64) float64 {
-	return packageSize * quantityInPackages * conversionFactor
+import "github.com/Global-Optima/zeep-web/backend/internal/data"
+
+func ConvertPackagesToUnits(ingredient data.StockRequestIngredient, quantityInPackages float64) float64 {
+	if ingredient.StockMaterial.Package.Unit.ID != ingredient.Ingredient.Unit.ID {
+		return ingredient.StockMaterial.Package.Size * quantityInPackages * ingredient.StockMaterial.Package.Unit.ConversionFactor
+	}
+
+	return ingredient.StockMaterial.Package.Size * quantityInPackages
+}
+
+type PackageMeasure struct {
+	QuantityInPackages float64 `json:"quantity"`        // Quantity of the packages
+	UnitsPerPackage    float64 `json:"unitsPerPackage"` // Number of units contained in a package
+	Unit               string  `json:"unit"`            // Name of the unit
+	TotalUnits         float64 `json:"totalUnits"`      // Total number of units
+
+}
+
+func ReturnPackageMeasure(ingredient data.StockRequestIngredient, quantityInPackages float64) PackageMeasure {
+	return PackageMeasure{
+		QuantityInPackages: quantityInPackages,
+		UnitsPerPackage:    ingredient.StockMaterial.Package.Size,
+		Unit:               ingredient.StockMaterial.Package.Unit.Name,
+		TotalUnits:         ConvertPackagesToUnits(ingredient, quantityInPackages),
+	}
 }
