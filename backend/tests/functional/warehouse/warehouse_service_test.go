2ac3a97640958c5cbcad5028f803ec7cf18c7f97
Dauletkhan Assanov <ab.dauletkhan@gmail.com>
2025-02-21 18:16:28 +0500

    added update tests

--- backend/tests/functional/warehouse/warehouse_service_test.go
+++ backend/tests/functional/warehouse/warehouse_service_test.go
@@ -66,6 +66,7 @@ func TestWarehouseService_GetWarehouseByID_WithPreloadedData(t *testing.T) {
 	}
 }
 
+// i will leave until getWarehouse and getAllWarehouse functions will be decided
 func TestWarehouseService_GetWarehouses_WithPreloadedData(t *testing.T) {
 	container := tests.NewTestContainer()
 	db := container.GetDB()
@@ -100,8 +101,69 @@ func TestWarehouseService_GetWarehouses_WithPreloadedData(t *testing.T) {
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			warehouses, err := module.Service.GetWarehouses(tc.filter)
-			assert.NoError(t, err)
-			assert.Len(t, warehouses, tc.expectedCount)
+			if tc.filter == nil {
+				assert.ErrorContains(t, err, fmt.Errorf("filter is nil").Error())
+			} else {
+				assert.NoError(t, err)
+				assert.Len(t, warehouses, tc.expectedCount)
+			}
+		})
+	}
+}
+
+func TestWarehouseService_UpdateWarehouse_WithPreloadedData(t *testing.T) {
+	container := tests.NewTestContainer()
+	db := container.GetDB()
+	module := tests.GetWarehouseModule()
+
+	if err := tests.TruncateAllTables(db); err != nil {
+		t.Fatalf("Failed to truncate all tables: %v", err)
+	}
+	if err := tests.LoadTestData(db); err != nil {
+		t.Fatalf("Failed to load test data: %v", err)
+	}
+
+	testCases := []struct {
+		name        string
+		id          uint
+		update      types.UpdateWarehouseDTO
+		expectError bool
+	}{
+		{
+			name: "Update existing Warehouse",
+			id:   1,
+			update: types.UpdateWarehouseDTO{
+				Name:              stringPtr("Primary Warehouse"),
+				RegionID:          uintPtr(1),
+				FacilityAddressID: uintPtr(1),
+			},
+			expectError: false,
+		},
+		{
+			name: "Update non-existing Warehouse",
+			id:   0,
+			update: types.UpdateWarehouseDTO{
+				Name:              stringPtr("Primary Warehouse"),
+				RegionID:          uintPtr(1),
+				FacilityAddressID: uintPtr(1),
+			},
+			expectError: true,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			warehouse, err := module.Service.UpdateWarehouse(tc.id, tc.update)
+			if tc.expectError {
+				assert.Error(t, err)
+				assert.Nil(t, warehouse)
+			} else {
+				assert.NoError(t, err)
+				assert.NotNil(t, warehouse)
+				assert.Equal(t, *tc.update.Name, warehouse.Name)
+				assert.Equal(t, tc.update.RegionID, uintPtr(warehouse.Region.ID))
+				// assert.Equal(t, tc.update.FacilityAddressID, warehouse.FacilityAddress) // need to think how to check
+			}
 		})
 	}
 }
