ae976a4df7b8517c9e5f6ca57088f1a06572b782
UnSk1LLe <104067408+UnSk1LLe@users.noreply.github.com>
2025-03-07 11:58:40 +0500

    remove unsed defaults

--- backend/internal/config/jwt.config.go
+++ backend/internal/config/jwt.config.go
@@ -2,13 +2,6 @@ package config
 
 import "time"
 
-const (
-	DEFAULT_CUSTOMER_ACCESS_TOKEN_TTL  = 1 * time.Hour
-	DEFAULT_CUSTOMER_REFRESH_TOKEN_TTL = 30 * 24 * time.Hour
-	DEFAULT_EMPLOYEE_ACCESS_TOKEN_TTL  = 15 * time.Minute
-	DEFAULT_EMPLOYEE_REFRESH_TOKEN_TTL = 24 * time.Hour
-)
-
 type JWTConfig struct {
 	CustomerSecretKey       string        `mapstructure:"JWT_CUSTOMER_SECRET_KEY" validate:"required"`
 	EmployeeSecretKey       string        `mapstructure:"JWT_EMPLOYEE_SECRET_KEY" validate:"required"`
