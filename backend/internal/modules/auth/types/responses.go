938a3ba2d28d53053a0016bcd4c1477a949479d4
Dauletkhan Assanov <80996807+ab-dauletkhan@users.noreply.github.com>
2025-03-19 02:31:28 +0500

    added (#260)
    Co-authored-by: Diar Begisbayev <93728467+DiarCode@users.noreply.github.com>

---
+++ backend/internal/modules/auth/types/responses.go
@@ -0,0 +1,8 @@
+package types
+
+import (
+	"github.com/Global-Optima/zeep-web/backend/internal/data"
+	"github.com/Global-Optima/zeep-web/backend/internal/localization"
+)
+
+var Response400IncorrectCredentials = localization.NewResponseKey(400, data.AuthenticationComponent)
