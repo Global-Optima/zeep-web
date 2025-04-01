8101c304020842765231ac457a1011eca7161d60
Diar Begisbayev <93728467+DiarCode@users.noreply.github.com>
2025-04-02 02:54:03 +0500

    feat: removed unusued order endpoints (#292)

--- frontend/src/modules/admin/store-orders/services/orders.service.ts
+++ frontend/src/modules/admin/store-orders/services/orders.service.ts
@@ -106,40 +106,6 @@ class OrderService {
 		}
 	}
 
-	async getSuborderBarcode(suborderId: number) {
-		try {
-			const response = await apiClient.get<Blob>(`/orders/suborders/${suborderId}/barcode`, {
-				responseType: 'blob',
-			})
-			return response.data
-		} catch (error) {
-			console.error(`Failed to fetch barcode for suborder ID ${suborderId}:`, error)
-			throw error
-		}
-	}
-
-	async getSuborderBarcodeFile(suborderId: number) {
-		try {
-			const response = await apiClient.get<Blob>(`/orders/suborders/${suborderId}/barcode`, {
-				responseType: 'blob',
-			})
-
-			return response.data
-		} catch (error) {
-			console.error(`Failed to fetch barcode for stock material ID ${suborderId}:`, error)
-			throw error
-		}
-	}
-
-	async completeSubOrder(orderId: number, subOrderId: number): Promise<void> {
-		try {
-			await apiClient.put(`/orders/${orderId}/suborders/${subOrderId}/complete`, {})
-		} catch (error) {
-			console.error('Failed to complete sub-order:', error)
-			throw error
-		}
-	}
-
 	async toggleNextSuborderStatus(subOrderId: number, options?: ToggleNextSuborderStatusOptions) {
 		try {
 			const response = await apiClient.put<SuborderDTO>(
@@ -150,62 +116,6 @@ class OrderService {
 			return response.data
 		} catch (error) {
 			console.error('Failed to change sub-order status:', error)
-			throw error
-		}
-	}
-
-	async completeSubOrderById(subOrderId: number) {
-		try {
-			const response = await apiClient.put<SuborderDTO>(
-				`/orders/suborders/${subOrderId}/complete`,
-				{},
-			)
-			return response.data
-		} catch (error) {
-			console.error('Failed to complete sub-order:', error)
-			throw error
-		}
-	}
-
-	async revertSubOrderById(subOrderId: number): Promise<void> {
-		try {
-			await apiClient.put(`/orders/suborders/${subOrderId}/revert`, {})
-		} catch (error) {
-			console.error('Failed to revert sub-order:', error)
-			throw error
-		}
-	}
-
-	async generatePDFReceipt(orderId: number): Promise<Blob> {
-		try {
-			const response = await apiClient.get(`/orders/${orderId}/receipt`, {
-				responseType: 'blob',
-			})
-			return response.data
-		} catch (error) {
-			console.error('Failed to generate PDF receipt:', error)
-			throw error
-		}
-	}
-
-	async getStatusesCount(): Promise<OrderStatusesCountDTO> {
-		try {
-			const response = await apiClient.get<OrderStatusesCountDTO>('/orders/statuses/count', {})
-			return response.data
-		} catch (error) {
-			console.error('Failed to fetch statuses count:', error)
-			throw error
-		}
-	}
-
-	async getSubOrderCount(orderId: number): Promise<number> {
-		try {
-			const response = await apiClient.get<{ count: number }>('/orders/suborders/count', {
-				params: { order_id: orderId },
-			})
-			return response.data.count
-		} catch (error) {
-			console.error('Failed to fetch sub-order count:', error)
 			throw error
 		}
 	}
