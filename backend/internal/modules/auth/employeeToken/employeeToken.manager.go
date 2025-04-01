19c49fbc72c200c9f51eee40bdb8241befe5974f
Ramazan Bakytuly <kuraikaroma@gmail.com>
2025-04-02 02:51:17 +0500

    chore: formatting

--- backend/internal/modules/auth/employeeToken/employeeToken.manager.go
+++ backend/internal/modules/auth/employeeToken/employeeToken.manager.go
@@ -53,7 +53,6 @@ func (r *employeeTokenManager) GetTokenByEmployeeID(employeeID uint) (*data.Empl
 		}).
 		Where("employee_id = ?", employeeID).
 		First(&token).Error
-
 	if err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
 			return nil, nil
