85b16fa62de0de106e5849cab9c9021c8f8325ee
Ramazan Bakytuly <kuraikaroma@gmail.com>
2025-04-08 00:34:16 +0500

    chore: formatting

--- backend/internal/modules/additives/technicalMap/types/responses.go
+++ backend/internal/modules/additives/technicalMap/types/responses.go
@@ -5,6 +5,4 @@ import (
 	"github.com/Global-Optima/zeep-web/backend/internal/localization"
 )
 
-var (
-	Response500TechnicalMapGet = localization.NewResponseKey(500, data.TechnicalMapComponent, data.GetOperation.ToString())
-)
+var Response500TechnicalMapGet = localization.NewResponseKey(500, data.TechnicalMapComponent, data.GetOperation.ToString())
