ec6d4e6ba4a95f0c6fd5a16bd0ff04b5d9b7064d
Ramazan Bakytuly <97844249+maulerrr@users.noreply.github.com>
2025-04-09 02:49:15 +0500

    feat: 404 & notification fix (#323)
    * feat: 404 & notification fix
    
    * chore: formatting
    
    * fix: linter
    
    ---------
    
    Co-authored-by: Diar Begisbayev <93728467+DiarCode@users.noreply.github.com>

--- backend/internal/modules/additives/technicalMap/technicalMap.handler.go
+++ backend/internal/modules/additives/technicalMap/technicalMap.handler.go
@@ -1,8 +1,10 @@
 package technicalMap
 
 import (
+	"errors"
+
 	"github.com/Global-Optima/zeep-web/backend/internal/localization"
-	"github.com/Global-Optima/zeep-web/backend/internal/modules/product/technicalMap/types"
+	"github.com/Global-Optima/zeep-web/backend/internal/modules/additives/technicalMap/types"
 	"github.com/Global-Optima/zeep-web/backend/pkg/utils"
 	"github.com/gin-gonic/gin"
 )
@@ -24,6 +26,11 @@ func (h *TechnicalMapHandler) GetAdditiveTechnicalMapByID(c *gin.Context) {
 
 	technicalMap, err := h.technicalMapService.GetAdditiveTechnicalMapByID(additiveID)
 	if err != nil {
+		if errors.Is(err, types.ErrorTechnicalMapNotFound) {
+			localization.SendLocalizedResponseWithKey(c, types.Response404TechnicalMap)
+			return
+		}
+
 		localization.SendLocalizedResponseWithKey(c, types.Response500TechnicalMapGet)
 		return
 	}
