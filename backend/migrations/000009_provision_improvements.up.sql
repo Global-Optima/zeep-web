6a99fa947f7cfece68b3fe06738f8b5affc512d3
UnSk1LLe <104067408+UnSk1LLe@users.noreply.github.com>
2025-04-15 18:32:36 +0500

    migrations fix (#340)

--- backend/migrations/000009_provision_improvements.up.sql
+++ backend/migrations/000009_provision_improvements.up.sql
@@ -1,5 +1,5 @@
 ALTER TABLE provisions
-    ADD COLUMN default_expiration_in_minutes INT NOT NULL CHECK (default_expiration_in_minutes > 0);
+    ADD COLUMN default_expiration_in_minutes INT NOT NULL CHECK (default_expiration_in_minutes > 0) DEFAULT 180;
 
 ALTER TABLE store_provisions DROP CONSTRAINT store_provisions_volume_check;
 
