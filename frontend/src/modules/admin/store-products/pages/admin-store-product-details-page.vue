a6a6ea68047431c4875a674845cb16a82e87b02a
Diar Begisbayev <93728467+DiarCode@users.noreply.github.com>
2025-04-15 06:57:37 +0500

    feat: added adjustments in Font naems, file names and layouting (#339)
    * feat: added adjustments in Font naems, file names and layouting
    
    * feat: fixed linters

--- frontend/src/modules/admin/store-products/pages/admin-store-product-details-page.vue
+++ frontend/src/modules/admin/store-products/pages/admin-store-product-details-page.vue
@@ -67,7 +67,7 @@ const updateMutation = useMutation({
 	},
 	onError: (error: AxiosLocalizedError) => {
 		toastLocalizedError(
-			error, 
+			error,
 			'Произошла ошибка при обновлении данных продукта кафе.',
 		)
 	},
